dd3833bc2f3c710902ac84b7dd15f14c1c208531
Sergio Lòpez <slopez.bloxity@gmail.com>
2019-04-24 11:07:24 -0500

    secod commit webservice

---
+++ ServiciosWeb.WebApi/Controllers/LibroController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+
+namespace ServiciosWeb.WebApi.Controllers
+{
+    public class LibroController : ApiController
+    {
+        //Libreriaconecction BD = new LibreriaConecction();
+
+        //public Enumerable<Libro> Get()
+        //{
+        //    var listado = BD.Libro.ToList();
+        //    return listado;
+        //}
+    }
+}
